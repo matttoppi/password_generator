6f8999168b61d1d2cca7a40a55316065454a4b78
username <email@blahblah.com>
2022-08-12 12:38:14 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# random password generator of a requested length
+
+import random
+import string
+
+
+def generate_password(length):
+    """
+    Generates a random password of length 'length'
+    :return:
+    password: string
+    """
+    password = ""
+    for i in range(0, length):
+        password += random.choice(string.ascii_letters + string.digits)
+    return password
+
+
+
+def __main__():
+    length = int(input("Enter the length of the password: "))
+    password = generate_password(length)
+    print(password)
+
+
+
+if __name__ == "__main__":
+    __main__()
